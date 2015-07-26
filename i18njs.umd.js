e1ca77f1027e78c840be2ee076949ca5bc851d9b
Yoann Moinet <yoann.moinet@gmail.com>
2015-07-26 16:42:29 -0400

    feat(build): use umd

--- i18njs.umd.js
+++ i18njs.umd.js
@@ -1,3 +1,4 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.i18njs = f()}})(function(){var define,module,exports;
 'use strict';
 /*-------------------------------------------*\
 |  TEMPLATE SYSTEM
@@ -235,3 +236,5 @@ var I18n = function () {
 module.exports = new I18n();
 
 return module.exports;
+
+});
